77dcb5d444ce0a01d1cf7ef3da0a488c6b2bdda5
Nguyen Thien Ly <nguyenmit2012@gmail.com>
2021-05-12 15:43:34 +0700

    update category route

--- src/config/roles.js
+++ src/config/roles.js
@@ -2,7 +2,7 @@ const roles = ['user', 'student', 'instructor', 'admin'];
 
 const roleRights = new Map();
 roleRights.set(roles[0], []);
-roleRights.set(roles[1], ['getUsers', 'manageUsers']);
+roleRights.set(roles[3], ['getUsers', 'manageUsers', 'getCategories', 'manageCategories']);
 
 module.exports = {
   roles,
